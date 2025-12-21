dde89f91da8d3ac4eb5c329855558301e66f1fe3
Aidan Gettemy <90980521+Aidan-Gettemy@users.noreply.github.com>
2025-12-21 13:55:53 -0600

    Add files via upload

--- BladeProfilePlot.m
+++ BladeProfilePlot.m
@@ -10,7 +10,7 @@ ls = 1; % if 0, do not see legend
 f = figure;
 f.Position = [50 50 1250 650];
 plot_blade(erosion_profile,s,ls)
-g = gca;g.FontSize = 14;g.FontName = 'Helvetica';
+g = gca;g.FontSize = 25;g.FontName = 'Helvetica';
 saveID = "Plots/"+"BladeRegionPlot.pdf";
 ax = gcf;
 exportgraphics(ax,saveID,'Resolution',300)
@@ -43,51 +43,51 @@ end
 
 % Add text
 if s == 0
-    a = 17;
+    a = 25;
     x1 = xregion(nodeLocs(2),nodeLocs(4),FaceColor=[.7 .7 .7],EdgeColor='k');
     x1.FaceAlpha = .1;
-    xtxt1 = text(nodeLocs(2)+.05,nodeHeights(2)*.9,'Cyl. 1','FontName','Ariel');
+    xtxt1 = text(nodeLocs(2)+.05,nodeHeights(2)*.9,'Cyl. 1','FontName','Helvetica');
     set(xtxt1,'Rotation',90)
     set(xtxt1,'FontSize',a)
     x2 = xregion(nodeLocs(4),nodeLocs(5),FaceColor=[.8 .8 .8],EdgeColor='k');
     x2.FaceAlpha = .1;
-    xtxt2 = text(nodeLocs(4)+.025,nodeHeights(4)*.9,'Cyl. 2','FontName','Ariel');
+    xtxt2 = text(nodeLocs(4)+.025,nodeHeights(4)*.9,'Cyl. 2','FontName','Helvetica');
     set(xtxt2,'Rotation',90)
     set(xtxt2,'FontSize',a)
     
     x3 = xregion(nodeLocs(5),nodeLocs(6),FaceColor='r',EdgeColor='k');
     x3.FaceAlpha = .1;
-    xtxt3 = text(nodeLocs(5)+.025,nodeHeights(5)*.9,'DU40','FontName','Ariel');
+    xtxt3 = text(nodeLocs(5)+.025,nodeHeights(5)*.9,'DU40','FontName','Helvetica');
     set(xtxt3,'Rotation',90)
     set(xtxt3,'FontSize',a)
 
     x4 = xregion(nodeLocs(6),nodeLocs(8),FaceColor='g',EdgeColor='k');
     x4.FaceAlpha = .1;
-    xtxt4 = text(nodeLocs(6)+.05,nodeHeights(6)*.9,'DU35','FontName','Ariel');
+    xtxt4 = text(nodeLocs(6)+.05,nodeHeights(6)*.9,'DU35','FontName','Helvetica');
     set(xtxt4,'Rotation',90)
     set(xtxt4,'FontSize',a)
 
     x5 = xregion(nodeLocs(8),nodeLocs(9),FaceColor='b',EdgeColor='k');
     x5.FaceAlpha = .1;
-    xtxt5 = text(nodeLocs(8)+.025,nodeHeights(8)*.9,'DU30','FontName','Ariel');
+    xtxt5 = text(nodeLocs(8)+.025,nodeHeights(8)*.9,'DU30','FontName','Helvetica');
     set(xtxt5,'Rotation',90)
     set(xtxt5,'FontSize',a)
 
     x6 = xregion(nodeLocs(9),nodeLocs(11),FaceColor='cyan',EdgeColor='k');
     x6.FaceAlpha = .1;
-    xtxt6 = text(nodeLocs(9)+.05,nodeHeights(9)*.9,'DU25','FontName','Ariel');
+    xtxt6 = text(nodeLocs(9)+.05,nodeHeights(9)*.9,'DU25','FontName','Helvetica');
     set(xtxt6,'Rotation',90)
     set(xtxt6,'FontSize',a)
 
     x7 = xregion(nodeLocs(11),nodeLocs(13),FaceColor='magenta',EdgeColor='k');
     x7.FaceAlpha = .1;
-    xtxt7 = text(nodeLocs(11)+.05,nodeHeights(11)*.9,'DU21','FontName','Ariel');
+    xtxt7 = text(nodeLocs(11)+.05,nodeHeights(11)*.9,'DU21','FontName','Helvetica');
     set(xtxt7,'Rotation',90)
     set(xtxt7,'FontSize',a)
 
     x8 = xregion(nodeLocs(13),nodeLocs(18),FaceColor='y',EdgeColor='k');
     x8.FaceAlpha = .1;
-    xtxt8 = text(nodeLocs(13)+.05,nodeHeights(13)*.9,'NACA 64','FontName','Ariel');
+    xtxt8 = text(nodeLocs(13)+.05,nodeHeights(13)*.9,'NACA 64','FontName','Helvetica');
     set(xtxt8,'Rotation',90)
     set(xtxt8,'FontSize',a)
 end
@@ -153,18 +153,18 @@ if ls == 1
             '','','','','','','','',...
             '','Region 1','','','','Region 2','',...
             '','','Region 3','','','','Region 4','','','','Region 5','','','','Region 6','','','','',...
-            'Aerodynamic Sensor','','','','');
+            'Sensor','','','','');
     else
         lgd = legend('','','','','','','','','','','','','','','','','',...
             '',...
             '','Region 1','','','','Region 2','',...
             '','','Region 3','','','','Region 4','','','','Region 5','','','','Region 6','','','','',...
-            'Aerodynamic Sensor','','','','');
+            'Sensor','','','','');
     end
-    lgd.FontSize = 14;lgd.FontName = 'Helvetica';
+    lgd.FontSize = 25;lgd.FontName = 'Helvetica';
     lgd.NumColumns = 1;lgd.FontWeight = "normal";
     lgd.Location = "northeast";
-    lgd.NumColumns = 2;
+    lgd.NumColumns = 1;
     
 end
 xlabel('r/R [percentage of total length]','FontSize',14,'FontWeight','normal','FontName','Helvetica')
